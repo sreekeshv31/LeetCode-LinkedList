7270501384eaa5be70080051df780cb8dafca1e5
Sreekesh V <61742339+sreekeshv31@users.noreply.github.com>
2020-11-15 13:44:04 +0530

    Add files via upload

---
+++ 7.Rotate list-Medium.cpp
@@ -0,0 +1,39 @@
+/*Given the head of a linked list, rotate the list to the right by k places.
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next || k==0)
+            return head;
+        ListNode *curr = head;
+        int len = 1;
+        while(curr->next && ++len)
+            curr = curr->next;
+        curr->next = head;
+        k = len-k%len;
+        while(k--)
+            curr = curr->next;
+        head = curr->next;
+        curr->next = nullptr;
+        return head;
+        
+    }
+};
+
+ 
